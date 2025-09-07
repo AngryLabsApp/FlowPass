2b6c929d9b3a3a65def547aa0d8d7d9ce2db854c
Grecia Delgado <greciadm2596@gmail.com>
2025-09-07 01:16:28 -0500

    update logout

--- public/src/login/logout.js
+++ public/src/login/logout.js
@@ -1,17 +1,16 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-  const BD_PUBLIC_URL = "https://nujwzmwnptrsqojdjyjl.supabase.co";
-  const BD_API_PUBLIC = "sb_publishable_xoykhydSmDArWUQydl_oLw_Ho0g1C64";
-  const LOGIN_PATH = "/login.html";
-  const supabase = createClient(BD_PUBLIC_URL, BD_API_PUBLIC);
+const BD_PUBLIC_URL = "https://nujwzmwnptrsqojdjyjl.supabase.co";
+const BD_API_PUBLIC = "sb_publishable_xoykhydSmDArWUQydl_oLw_Ho0g1C64";
+const LOGIN_PATH = "/login.html";
+const supabase = createClient(BD_PUBLIC_URL, BD_API_PUBLIC);
 
-
-  document.getElementById("logout").addEventListener("click", async () => {
-    const { error } = await supabase.auth.signOut();
-    if (error) {
-      console.error("Error al cerrar sesión:", error.message);
-      return;
-    }
-    // opcional: limpia algo propio de tu app aquí
-    window.location.replace(LOGIN_PATH);
-  });
+document.addEventListener("DOMContentLoaded", async () => {
+  const { error } = await supabase.auth.signOut();
+  if (error) {
+    console.error("Error al cerrar sesión:", error.message);
+    return;
+  }
+  // opcional: limpia algo propio de tu app aquí
+  window.location.replace(LOGIN_PATH);
+});
