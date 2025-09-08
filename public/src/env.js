163e52db9a8e9c71698a948857c45f543b2f8788
AngelEVHZ <angel_evh@hotmail.com>
2025-09-08 17:19:55 -0600

    actuliza el formulario

--- public/src/env.js
+++ public/src/env.js
@@ -11,7 +11,7 @@ const IMAGES_PATH_CHECKIN = {
 
 const ENV_VARS = {
   url_form:
-    "https://n8n.angrylabs.app/form/54f40a75-b183-4483-9c51-82d281c6b504",
+    "https://n8n.angrylabs.app/form/3858af95-5038-4d61-ba84-4ee767accee2",
   url_get_users:
     "https://n8n.angrylabs.app/webhook/9ecc1791-c157-4084-8de6-6924235d95cd",
   url_update:
